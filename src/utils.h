59fe4e95d977a72d175af704f23411d1adc8379e
Lei <lynnl.yet@gmail.com>
2019-01-24 10:02:43 +0800

    Add LOG_DBG LOG_ERR

--- src/utils.h
+++ src/utils.h
@@ -7,7 +7,13 @@
 #define PLUGIN_NAME_S "XXX"
 #endif
 
-#define LOG(fmt, ...) NSLog(@PLUGIN_NAME_S ": " fmt "\n", ##__VA_ARGS__)
+#define LOG(fmt, ...)       NSLog(@PLUGIN_NAME_S ": " fmt "\n", ##__VA_ARGS__)
+#define LOG_ERR(fmt, ...)   LOG("[ERR] " fmt, ##__VA_ARGS__)
+#ifdef DEBUG
+#define LOG_DBG(fmt, ...)   LOG("[DBG] " fmt, ##__VA_ARGS__)
+#else
+#define LOG_DBG(fmt, ...)   (void) (0, ##__VA_ARGS__)
+#endif
 
 #endif	/* UTILS_H */
 
